9237cceb7a45d4131812eeb21046f135fd121978
Owner <wassoval000@warren.k12.in.us>
2018-03-27 10:39:44 -0400

    Initial commit

--- src/Attack.java
+++ src/Attack.java
@@ -3,17 +3,18 @@ import java.awt.*;
 public class Attack extends Sprite {
 
     static long time = System.currentTimeMillis();
+    static boolean moving = true;
     static int LRSPEED = 8;
     static int UDSPEED = 8;
 
-    public Attack(Color color, int x, int y, int width, int height){
+    public Attack(Color color, int x, int y, int width, int height) {
 
         super(color, x, y, width, height);
 
     }
 
     @Override
-    public void paint(Graphics g){
+    public void paint(Graphics g) {
 
         g.setColor(color);
         g.fillRect(x, y, width, height);
@@ -23,26 +24,32 @@ public class Attack extends Sprite {
     public void move(int boardHeight, int boardWidth) {
 
         long changeTime = System.currentTimeMillis() - time;
-        if (changeTime < 5000) {
+        if (changeTime > 5000) {
+            moving = !moving;
+            time = System.currentTimeMillis();
+        }
+
+        if(moving){
             x += LRSPEED;
             if (x > boardWidth) {
+                x -= 10;
                 LRSPEED *= -1;
             }
             if (x < 0) {
+                x += 10;
                 LRSPEED *= -1;
             }
         }
-        if (changeTime < 10000 && changeTime > 5000) {
+        else{
             y += UDSPEED;
             if (y > 600) {
+                y -= 10;
                 UDSPEED *= -1;
             }
             if (y < 0) {
+                y += 10;
                 UDSPEED *= -1;
             }
         }
-
-
     }
-
-    }
+}
--- src/Sprite.java
+++ src/Sprite.java
@@ -32,6 +32,25 @@ public class Sprite implements Move, Paint{
 
     }
 
+    public void bounce(){
+
+        dx *= -1;
+        dy *= -1;
+
+    }
+
+    public boolean intersects (Sprite other){
+
+        return this.getBounds().intersects(other.getBounds());
+
+    }
+
+    public boolean visibilityS (){
+
+        return false;
+
+    }
+
     @Override
     public void setPosition(int x, int y) {
 
