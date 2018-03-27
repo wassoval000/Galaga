e0a89e033103d618d6e325bc947117fe6ba14ba5
Owner <wassoval000@warren.k12.in.us>
2018-03-26 20:14:34 -0400

    Initial commit

--- src/Attack.java
+++ src/Attack.java
@@ -23,25 +23,25 @@ public class Attack extends Sprite {
     public void move(int boardHeight, int boardWidth){
 
         long changeTime = System.currentTimeMillis()-time;
-        if(changeTime < 5000){
-            x += LRSPEED;
-            if(x>boardWidth){
-                LRSPEED *= -1;
-            }
-            if(x<0){
-                LRSPEED *= -1;
-            }
+            if (changeTime < 5000) {
+                x += LRSPEED;
+                if (x > boardWidth) {
+                    LRSPEED *= -1;
+                }
+                if (x < 0) {
+                    LRSPEED *= -1;
+                }
 
-        }
-        if(changeTime < 10000 && changeTime > 5000){
-            y += UDSPEED;
-            if(y>700){
-             UDSPEED *= -1;
             }
-            if(y<boardHeight){
-                UDSPEED *= -1;
+            if (changeTime < 10000 && changeTime > 5000) {
+                y += UDSPEED;
+                if (y > 600) {
+                    UDSPEED *= -1;
+                }
+                if (y < 0) {
+                    UDSPEED *= -1;
+                }
             }
-        }
 
     }
 
